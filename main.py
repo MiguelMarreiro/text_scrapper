674a39c9b50f86b32f84164bbcd5edd7aa7fb1ef
MiguelMarreiro <miguel.marreiro>
2024-09-05 16:55:20 +0100

    add file selection line and automatically recognize image extension

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import openai
-from os import environ
+from os import environ, path
 import dotenv
 import base64
 
@@ -10,7 +10,13 @@ dotenv.load_dotenv()
 
 client = openai.OpenAI()
 THIS_MODEL = "gpt-4o-mini"
+IMAGE_PATH = 'source/font_emotions.png'
 
+image_name, image_type = path.splitext(IMAGE_PATH)
+# Removing the dot from the extension
+clean_extension = image_type.lstrip('.')
+output_name = image_name.lstrip('source/')
+print(clean_extension)  # Output: extension
 
 # Function to encode the image
 def encode_image(image_path):
@@ -19,7 +25,7 @@ def encode_image(image_path):
 
 
 # Getting the base64 string
-base64_image = encode_image('source/font_emotions.png')
+base64_image = encode_image(IMAGE_PATH)
 
 
 # Send the request to the API
@@ -46,7 +52,7 @@ response = client.chat.completions.create(
                         "type": "image_url",
                         "image_url":
                             {
-                                "url": f"data:image/png;base64,{base64_image}"
+                                "url": f"data:image/{clean_extension};base64,{base64_image}"
                             }
                     }
                 ]
@@ -59,4 +65,5 @@ print(f"response: {response}")
 description = response.choices[0].message.content
 print(f"Desription: {description}")
 
-
+with open(f'output/{output_name}.txt', 'w', encoding='utf-8') as out_file:
+    out_file.write(description)
