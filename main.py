add82f8dd572bd1448350006b093329ee2eb1fbf
MiguelMarreiro <miguel.marreiro>
2024-09-05 17:08:30 +0100

    add file selector GUI

--- main.py
+++ main.py
@@ -3,26 +3,54 @@ from os import environ, path
 import dotenv
 import base64
 
+import tkinter as tk
+from tkinter import filedialog
 
+"""Load Keys"""
 dotenv.load_dotenv()
-
 # print(environ['OPENAI_API_KEY'])
 
+
+def select_file():
+    """Creates a guy file selector which returns the file path for the selected file"""
+    root = tk.Tk()
+    root.withdraw()  # Hide the root window
+
+    # Open a file dialog and ask the user to select a file
+    file_path = filedialog.askopenfilename()
+
+    # Print the selected file path
+    if file_path:
+        return file_path
+    else:
+        print("No file selected.")
+
+
+
+# Function to encode the image
+def encode_image(image_path):
+    """Encodes the image in base64 so it can be sent with the message text"""
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+""" Initiates the openAI connection"""
 client = openai.OpenAI()
 THIS_MODEL = "gpt-4o-mini"
-IMAGE_PATH = 'source/font_emotions.png'
+
+
+#_______________Path selection___________________
+
+IMAGE_PATH = select_file()
+
+# IMAGE_PATH = 'source/font_emotions.png'
 
 image_name, image_type = path.splitext(IMAGE_PATH)
 # Removing the dot from the extension
 clean_extension = image_type.lstrip('.')
-output_name = image_name.lstrip('source/')
+output_name = image_name.lstrip('D:/Projects/OCR/openai/source/')
 print(clean_extension)  # Output: extension
-
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
-
+print(output_name)
 
 # Getting the base64 string
 base64_image = encode_image(IMAGE_PATH)
